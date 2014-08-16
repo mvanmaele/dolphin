d6c8342ebbd91afe37139b5dc256cdb644bb2f19
mvanmaele <mvanmaele@gryphonscientific.com>
2014-08-16 15:31:56 -0400

    Added AJAX stuff.

--- js/bundle.js
+++ js/bundle.js
@@ -1,4 +1,21 @@
-﻿// initialize Google Visualizations package.
+﻿///#source 1 1 /js/utility.js
+// formats back-end data so its a nice-looking object
+function formatData(rawData) {
+    var nrow = rawData.tf.length;
+    var ncol = rawData.tf[0].c.length;
+    var niceData = [];
+    for (var i = 1; i < nrow; i++) {
+        var newObj = {};
+        for (var j = 0; j < ncol; j++) {
+            var colName = rawData.tf[0].c[j].v;
+            newObj[colName] = rawData.tf[i].c[j].v;
+        }
+        niceData.push(newObj);
+    }
+    return niceData;
+}
+///#source 1 1 /js/app.js
+// initialize Google Visualizations package.
 google.load("visualization", '1', { packages: ['table'] });
 google.load("gdata", "2");
 google.setOnLoadCallback(getMyFeed);
---
+++ js/utility.js
@@ -0,0 +1,15 @@
+﻿// formats back-end data so its a nice-looking object
+function formatData(rawData) {
+    var nrow = rawData.tf.length;
+    var ncol = rawData.tf[0].c.length;
+    var niceData = [];
+    for (var i = 1; i < nrow; i++) {
+        var newObj = {};
+        for (var j = 0; j < ncol; j++) {
+            var colName = rawData.tf[0].c[j].v;
+            newObj[colName] = rawData.tf[i].c[j].v;
+        }
+        niceData.push(newObj);
+    }
+    return niceData;
+}
